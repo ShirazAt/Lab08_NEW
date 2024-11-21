4351a9207713d8e6ef96bd52c8a401127f00bf21
Shiraz <shatamna@gmail.com>
2024-11-21 12:03:55 -0500

    fixed tests

--- app/src/main/java/com/example/lab8/PasswordValidator.java
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -8,13 +8,17 @@ public class PasswordValidator {
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
 
+        boolean hasNumber= false;
+        boolean isMoreThan10 = false;
+
+        if (password.length() >=10) isMoreThan10 = true;
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpperCase = true;
             if (Character.isLowerCase(c)) hasLowerCase = true;
-
-            // If both conditions are met, the password is valid
-            if (hasUpperCase && hasLowerCase) return true;
+            if (Character.isDigit(c)) hasNumber = true;
         }
+
+        if (hasUpperCase && hasLowerCase && hasNumber && isMoreThan10) return true;
         return false;
     }
 }
